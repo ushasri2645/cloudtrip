d6d8a6b0bd114aa35e19d84924fe00842b3d53f5
NikithaMedikonda <nikitha.medikonda@everest.engineering>
2025-07-07 15:24:13 +0530

    Add import dynamic pricing service

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,10 +1,11 @@
+require_relative "../services/dynamic_pricing_service"
+
 class FlightsController < ApplicationController
   DATA_PATH = Rails.configuration.flight_data_file
-  def index
-  end
 
- def search
-    @cities = [ "Bangalore", "Chennai", "Delhi", "Mumbai", "London", "New York" ]
+  def index; end
+
+  def search
     source = params[:source]
     destination = params[:destination]
     date = params[:date]
@@ -21,24 +22,45 @@ class FlightsController < ApplicationController
             [ flight[:business_seats], 1.5 ]
         when "first_class"
             [ flight[:first_class_seats], 2.0 ]
-        else
-            [ 0, 1.0 ]
         end
 
       flight[:source].casecmp?(source) &&
       flight[:destination].casecmp?(destination) &&
       flight[:date] == date &&
       seats_available >= passengers
+    end.map do |flight|
+      seat_key = "#{class_type}_seats".to_sym
+      available_seats = flight[seat_key]
+      total_seats =
+        case class_type
+        when "economy"     then flight[:economy_total]
+        when "business"    then flight[:business_total]
+        when "first_class" then flight[:first_class_total]
+        end
+
+      dynamic_price = DynamicPricingService.calculate_price(
+        flight[:price],
+        total_seats,
+        available_seats
+      )
+
+      price_per_person = (dynamic_price - flight[:price])  + (flight[:price] * price_multiplier)
+      total_fare = price_per_person * passengers
+
+
+      flight.merge(
+        total_fare: total_fare,
+        price_per_seat: dynamic_price,
+        price_per_person: price_per_person
+      )
     end
-    .map do |flight|
-        total_fare = flight[:price] * price_multiplier * passengers
-        flight.merge(total_fare: total_fare)
-    end
+
     flash.now[:alert] = "No Flights Available" if @matching_flights.empty?
     render :index
   end
 
   private
+
   def read_flights
     File.readlines(DATA_PATH).map do |line|
       fields = line.strip.split(",")
@@ -53,7 +75,10 @@ class FlightsController < ApplicationController
         price: fields[7].to_f,
         economy_seats: fields[8].to_i,
         business_seats: fields[9].to_i,
-        first_class_seats: fields[10].to_i
+        first_class_seats: fields[10].to_i,
+        economy_total: fields[11].to_i,
+        business_total: fields[12].to_i,
+        first_class_total: fields[13].to_i
       }
     end
   end
