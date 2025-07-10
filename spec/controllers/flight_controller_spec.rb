7f05e45344ad9a3ea3c4ded12f60670b112ebc1e
ushasri2645 <ugudikandula@everest.engineering>
2025-07-10 11:31:39 +0530

    Change the text Origin to Source

--- spec/controllers/flight_controller_spec.rb
+++ spec/controllers/flight_controller_spec.rb
@@ -169,7 +169,7 @@ DATA
         }
 
         expect(response).to have_http_status(:ok)
-        expect(response.body).to include("Origin and Destination must be different.")
+        expect(response.body).to include("Source and Destination must be different.")
         expect(response.body).not_to include("F101")
         expect(response.body).not_to include("F103")
       end
