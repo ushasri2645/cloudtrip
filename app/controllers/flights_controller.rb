0c325f0b9e5460838271b4b7d8e4ff4481af58d4
jangamkeerthi26 <keerthi.jangam@everest.engineeing>
2025-07-10 11:31:06 +0530

    Fix: Ensure time zone consistency when comparing flight departure time

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -53,7 +53,7 @@ class FlightsController < ApplicationController
         if Date.parse(date) == Time.zone.today
           now = Time.zone.now
           flight_datetime_str = "#{flight[:departure_date]} #{flight[:departure_time]}"
-          flight_time = Time.strptime(flight_datetime_str, "%Y-%m-%d %I:%M %p")
+          flight_time = Time.zone.strptime(flight_datetime_str, "%Y-%m-%d %I:%M %p")
           time_condition = flight_time > now
         end
 
