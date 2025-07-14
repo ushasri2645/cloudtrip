6cb5fd0d2d95d91c67f75914639081df7eef3849
jangamkeerthi26 <keerthi.jangam@everest.engineeing>
2025-07-14 13:05:12 +0530

    Add method to read and parse flight data from file into structured hashes

--- app/services/flight_data_service.rb
+++ app/services/flight_data_service.rb
@@ -6,4 +6,24 @@ class FlightDataService
       [ fields[1], fields[2] ]
     end.flatten.uniq.sort
   end
+
+  def self.read_flights
+    File.readlines(DATA_PATH).map do |line|
+      fields = line.strip.split(",")
+      {
+        flight_number:      fields[0],
+        source:             fields[1],
+        destination:        fields[2],
+        departure_date:     fields[3],
+        departure_time:     fields[4],
+        price:              fields[5].to_f,
+        economy_seats:      fields[6].to_i,
+        economy_total:      fields[7].to_i,
+        business_seats:     fields[8].to_i,
+        business_total:     fields[9].to_i,
+        first_class_seats:  fields[10].to_i,
+        first_class_total:  fields[11].to_i
+      }
+    end
+  end
 end
