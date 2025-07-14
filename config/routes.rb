0f6a29aa434b7bf0d3c3e024121634e478ea5cba
jangamkeerthi26 <keerthi.jangam@everest.engineeing>
2025-07-14 13:05:31 +0530

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
-
   root "flights#index"
+
   post "flights/search", to: "flights#search"
   get "flights/search", to: "flights#search"
   post "flights/book", to: "flights#book"
 
   namespace :api do
     get "cities", to: "flights#cities"
+    post "flights", to: "flights#search"
   end
 end
