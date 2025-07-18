377797e56e82136b00a8a491110b64f41c1fde9a
jangamkeerthi26 <keerthi.jangam@everest.engineeing>
2025-07-18 12:27:20 +0530

    Add route for fetching cities from DB

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,8 @@
 Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
   root "flights#index"
-
-  post "flights/search", to: "flights#search"
-  get "flights/search", to: "flights#search"
-  post "flights/book", to: "flights#book"
-
   namespace :api do
-    get "cities", to: "flights#cities"
+    get "cities", to: "airports#cities"
     post "flights", to: "flights#search"
     post "book", to: "flights#book"
   end
