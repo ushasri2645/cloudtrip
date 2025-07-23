e00d53a9b2f053a03ded8cbddc3cf72b333ebc22
ushasri2645 <ugudikandula@everest.engineering>
2025-07-23 08:56:38 +0530

    Ensure table is empty before writing assertions

--- spec/requests/api/airports_spec.rb
+++ spec/requests/api/airports_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe "Api::Airports", type: :request do
   describe "GET get /api/cities" do
     before(:all) do
+      Airport.delete_all
       @airport1 = Airport.create!(city: "Delhi", code: "DEL")
       @airport2 = Airport.create!(city: "Mumbai", code: "BOM")
     end
