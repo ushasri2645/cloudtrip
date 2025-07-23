648376c4aaf52c5005fb21f01b47b3246770ae48
ushasri2645 <ugudikandula@everest.engineering>
2025-07-23 08:39:14 +0530

    Modify the validations and associations with newly created models

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -2,19 +2,11 @@ class Flight < ApplicationRecord
   belongs_to :source, class_name: "Airport"
   belongs_to :destination, class_name: "Airport"
 
-  has_many :flight_seats, dependent: :destroy
+  has_many :base_flight_seats, dependent: :destroy
   has_many :seat_classes, through: :flight_seats
-
-  has_many :class_pricings, dependent: :destroy
+  has_one :flight_recurrence, dependent: :destroy
+  has_many :flight_schedules, dependent: :destroy
 
   validates :flight_number, presence: true
-  validates :departure_datetime, :arrival_datetime, presence: true
-  validates :total_seats, numericality: { greater_than_or_equal_to: 0 }
-  validates :price, numericality: { greater_than_or_equal_to: 0 }
-  validate  :arrival_after_departure
-
-  def arrival_after_departure
-    return if arrival_datetime.blank? || departure_datetime.blank?
-    errors.add(:arrival_datetime, "must be after departure") if arrival_datetime <= departure_datetime
-  end
+  validates :departure_time,  presence: true
 end
