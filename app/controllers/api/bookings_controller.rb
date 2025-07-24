feaef4a0d9767e5caa1b1706bf73fcb20da8d8da
ushasri2645 <ugudikandula@everest.engineering>
2025-07-24 16:38:42 +0530

    Add controller by using updated validator and service

--- app/controllers/api/bookings_controller.rb
+++ app/controllers/api/bookings_controller.rb
@@ -2,27 +2,35 @@ module Api
   class BookingsController < ApplicationController
     def booking
       flight_number, class_type, passengers, source, destination, departure_date, departure_time = extract_booking_params
-      params_for_service = {
-  flight_number: flight_number,
-  class_type: class_type,
-  passengers: passengers,
-  source: source,
-  destination: destination,
-  date: departure_date
-}
 
-result = FlightBookingService.new(params_for_service).book_flight
-      if !result[:success]
-        render json: { updated: false, error: result[:message] }, status: :unprocessable_entity
+      params = {
+        flight_number: flight_number,
+        class_type: class_type,
+        passengers: passengers,
+        source: source,
+        destination: destination,
+        date: departure_date
+      }
+      validator = FlightBookingValidator.new(params)
+
+      unless validator.valid?
+        return render json: { updated: false, errors: validator.errors }, status: :bad_request
+      end
+
+      booking_service = FlightBookingService.new(
+        schedule: validator.schedule,
+        seat: validator.seat,
+        passengers: params[:passengers].to_i
+      )
+      bookingResult = booking_service.book_flight
+
+      if bookingResult[:success]
+        render json: { updated: true, message: bookingResult[:message], data: bookingResult[:data] }, status: :ok
       else
-        render json: {
-          updated: true,
-          message: "Booking successful",
-          flight: result[:flight],
-          seat_class: result[:seat_class]
-        }, status: :ok
+        render json: { updated: false, error: bookingResult[:message] }, status: bookingResult[:status] || :unprocessable_entity
       end
     end
+
     def extract_booking_params
       flight_params   = params[:flight] || {}
       flight_number   = flight_params[:flight_number]
