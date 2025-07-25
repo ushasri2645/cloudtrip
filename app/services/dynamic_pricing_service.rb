f0a922c61a709245b0ef71d11dac4af86ee7fb7e
jangamkeerthi26 <keerthi.jangam@everest.engineering>
2025-07-25 12:08:01 +0530

    Split dynamic pricing stratergy to mutiple stratergies

--- app/services/dynamic_pricing_service.rb
+++ app/services/dynamic_pricing_service.rb
@@ -1,5 +1,23 @@
 class DynamicPricingService
   def self.calculate_price(base_price, total_seats, available_seats, flight_date_str)
+    strategies = [
+      SeatBasedPricing.new,
+      DateBasedPricing.new
+    ]
+
+    strategies.sum do |strategy|
+      strategy.calculate(
+        base_price: base_price,
+        total_seats: total_seats,
+        available_seats: available_seats,
+        flight_date: flight_date_str
+      )
+    end
+  end
+end
+
+class SeatBasedPricing
+  def calculate(base_price:, total_seats:, available_seats:, **)
     seats_sold = total_seats - available_seats
     seat_ratio = seats_sold.to_f / total_seats
 
@@ -13,19 +31,26 @@ class DynamicPricingService
       else
         0.5
       end
-
-    seat_based_dynamic_price = base_price * seat_multiplier
-    date_based_dynamic_price = 0.0
-    flight_date = Date.parse(flight_date_str) rescue nil
-    if flight_date
-      days_left = (flight_date - Time.zone.today).to_i
-      if days_left <= 15 && days_left >= 3
-        date_based_dynamic_price = base_price * (0.02 * (15 - days_left))
-      elsif days_left < 3  && days_left >= 0
-        date_based_dynamic_price = base_price * (0.15 * (3 - days_left))
-      end
-    end
-    final_price = seat_based_dynamic_price + date_based_dynamic_price
-    final_price
+    base_price * seat_multiplier
   end
 end
+
+
+class DateBasedPricing
+  def calculate(base_price:, flight_date:, **)
+    days_left = (flight_date - Time.zone.today).to_i
+    if days_left <= 15 && days_left >= 3
+      base_price * (0.02 * (15 - days_left))
+    elsif days_left < 3 && days_left >= 0
+      base_price * (0.15 * (3 - days_left))
+    else
+      0
+    end
+  end
+end
+
+
+
+
+
+
