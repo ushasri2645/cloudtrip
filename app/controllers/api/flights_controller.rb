6be2a835b7d42ceaaa2312d146f5e5df30967a75
jangamkeerthi26 <keerthi.jangam@everest.engineering>
2025-07-30 13:34:35 +0530

    Handle status codes in the controller it self

--- app/controllers/api/flights_controller.rb
+++ app/controllers/api/flights_controller.rb
@@ -7,17 +7,34 @@ module Api
         return render json: { errors: validator.errors }, status: :bad_request
       end
 
-      search_service = FlightSearchService.new(
-        params[:source], params[:destination], params[:date],
-        validator.class_type, validator.passengers
-      )
+      service = FlightSearchService.new(validator)
+      result = service.search
 
-      search_results = search_service.search_flights
+      case result
+      when :invalid_class_type
+        render json: { message: "Invalid class type" }, status: :bad_request
 
-      render json: {
-        flights: search_results[:flights],
-        message: search_results[:message]
-      }, status: search_results[:status]
+      when :invalid_airports
+        render json: { message: "Source or destination not found." }, status: :bad_request
+
+      when :route_not_operated
+        render json: { message: "We are not operating on this route. Sorry for the inconvenience" }, status: :not_found
+
+      when :no_flights_on_date
+        render json: { flights: [], message: "No available flights on this date" }, status: :ok
+
+      when :no_class_available
+        render json: { message: "Sorry! 😔 No flights available for #{validator.class_type}." }, status: :not_found
+
+      when :all_seats_booked
+        render json: { message: "All seats are booked in #{validator.class_type} class on #{validator.parsed_date}" }, status: :conflict
+
+      else
+        render json: {
+          flights: result,
+          message: "Flights found"
+        }, status: :ok
+      end
     end
   end
 end
